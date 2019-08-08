58efe09e652c180e626e18b85e070d92c03768c7
Brad <brad@brad.tw>
2019-08-08 19:37:58 +0800

    3

--- app/src/main/java/tw/org/tcca/app/webviewtest1/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/webviewtest1/MainActivity.java
@@ -2,8 +2,12 @@ package tw.org.tcca.app.webviewtest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -18,9 +22,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initWebView(){
+        WebViewClient client = new WebViewClient();
+        webView.setWebViewClient(client);
 
-        webView.loadUrl("file:///android_asset/brad.html");
+
+
+        webView.loadUrl("https://www.iii.org.tw");
+
+        //webView.loadUrl("file:///android_asset/brad.html");
+
+        //String data = "<input type='text'><hr><select><option>item1</option><option>item1</option></select>";
+        //webView.loadData(data, "text/html; charset=utf-8", null);
 
     }
 
+    public void test1(View view) {
+        Uri uri = Uri.parse("https://www.iii.org.tw");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+
+    }
 }
