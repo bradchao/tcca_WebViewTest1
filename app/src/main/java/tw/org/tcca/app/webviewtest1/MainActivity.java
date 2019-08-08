e22d07a827119392d499e33dd8c4b764da569b4e
Brad <brad@brad.tw>
2019-08-08 19:08:01 +0800

    2

--- app/src/main/java/tw/org/tcca/app/webviewtest1/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/webviewtest1/MainActivity.java
@@ -3,12 +3,24 @@ package tw.org.tcca.app.webviewtest1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        initWebView();
+
     }
+
+    private void initWebView(){
+
+        webView.loadUrl("file:///android_asset/brad.html");
+
+    }
+
 }
